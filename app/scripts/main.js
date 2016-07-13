820db064fc9b503fe1546fd6260c5c8898b4cba4
Sharynne Azhar <sharynneazhar@gmail.com>
2016-07-12 22:53:34 -0500

    scaffold the app -
    * add the search bar UI component
    * add mock data
    * add in color palette
    * organize project structure

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,6 @@
-console.log('\'Allo \'Allo!');
+var mockData = '../mocks/addresses.json';
+$.getJSON(mockData, function(data) {
+  console.log(data.home);
+  console.log(data.work);
+  console.log(data.school);
+});
