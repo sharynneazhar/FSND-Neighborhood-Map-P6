5acad446fd8965a3ba6f4af426f7ced95a1f9d58
Sharynne Azhar <sharynne@mobilgov.com>
2016-07-13 21:47:45 -0500

    pull out constants to a separate file

---
+++ app/scripts/environmentVars.js
@@ -0,0 +1,4 @@
+const DEFAULT_LOC = [39.0997, -94.5786];
+
+const GOOGLE_API_KEY = 'AIzaSyCKioYCg26ODl5A4Z2K03OFkXJRT1mBpRk';
+const GOOGLE_BASE_URL = 'http://maps.googleapis.com/maps/api/js?key=' + GOOGLE_API_KEY;
